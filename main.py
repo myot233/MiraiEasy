9b02942f75f69d121a7fed1baf93f56595a48e9c
gsycl2004 <6548308+gsycl2004@user.noreply.gitee.com>
2020-09-21 20:04:10 +0800

    ok

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import time
 import sys
 import easygui
 import configparser
+
+
 class main():
 	def __init__(self):
-
-
 		pass
 
 	def main(self):
 		che = check()
+		print("如启动后报错，请删除java.ini")
 		che.Checkmethod()
 
 		# 检测文件
@@ -32,9 +33,13 @@ class check():
 		if os.path.exists(self.cwd + '/libs'):
 			print("已检测到文件夹")
 			if self.checkjar():
-				if not os.path.exists(self.cwd+"/java.ini"):
-					s = easygui.diropenbox('选择java所在的文件夹','文件对话框')
+				if not os.path.exists(self.cwd + "/java.ini"):
+					s = easygui.diropenbox('选择java所在的文件夹', '文件对话框')
 					lt = s + '\\java.exe'
+					p = configparser.ConfigParser()
+					p.add_section('java')
+					p.set('java', 'java',lt)
+					p.write(open("java.ini", 'w+'))
 					print(lt)
 					sth = os.system(f'''{lt} -cp "./libs/*" net.mamoe.mirai.console.pure.MiraiConsolePureLoader %*''')
 					os.system('title Mirai Console')
@@ -42,22 +47,27 @@ class check():
 
 					pass
 				else:
+
 					p = configparser.ConfigParser()
-					p.read(self.cwd+"/java.ini",encoding='utf-8')
-					lt = p['java']['path']
+					p.read(self.cwd + "/java.ini", encoding='utf-8')
+					lt = p['java']['java']
+					sth = os.system(f'''{lt} -cp "./libs/*" net.mamoe.mirai.console.pure.MiraiConsolePureLoader %*''')
+					os.system('title Mirai Console')
+					print(sth)
 			else:
 
 				self.getjar()
 		else:
 			os.mkdir(self.cwd + "\\libs")
 			print('正在创建文件中...')
-			self.result = [False,False,False]
+			self.result = [False, False, False]
 			self.getjar()
 
 	def getjar(self):
 		print(1)
 		adm = 0
 		dir1 = self.cwd + "\\libs"
+
 		def downloadFile(name, url):
 			headers = {'Proxy-Connection': 'keep-alive'}
 			r = requests.get(url, stream=True, headers=headers)
@@ -77,22 +87,26 @@ class check():
 						print(name + ': ' + formatFloat(p) + '%' + ' Speed: ' + formatFloat(speed) + 'M/S')
 						time1 = time.time()
 			f.close()
+
 		def formatFloat(num):
 			return '{:.2f}'.format(num)
+
 		for s in self.result:
 			if s == False and adm == 0:
-				#print(1)
-				downloadFile('libs\mirai-core-qqandroid-1.2.3.jar','https://dev25.baidupan.com/092015bb/2020/09/20/dc92de2f98b8796c920cfcac93537340.jar?st=34hxIiJ3VkSU7so-W9gavw&e=1600589246&b=AT5ZMFIgUTUCPlIpUWIBagYhCzIBLVAnAyFbN1w1XjhTc10yUDlZOFggVzBWKAUzUSpdMld4VzsFNwF_b&fi=29928242&pid=218-83-1-137&up=1.')
+				# print(1)
+				downloadFile('libs\mirai-core-qqandroid-1.2.3.jar',
+				             'https://raw.githubusercontent.com/project-mirai/mirai-repo/master/shadow/mirai-core-qqandroid/mirai-core-qqandroid-1.2.3.jar')
 			if s == False and adm == 1:
-				downloadFile('libs\mirai-console-pure-1.0-M4-dev-3.jar','https://dev05.baidupan.com/092015bb/2020/09/20/30c02e7824542722fbd1bb8c65faf847.jar?st=XCiFdJCTNX7ofNfHJPF8FQ&e=1600589005&b=BzgIYQl7UTVSbgV_bAjFXPFNoWnUEalc9VWMIKFUiVSJTcwhtBisEMAJ5UDZRLFIbVDVbKgFkC2gCJwAgBjICLgc_aCGkJew_c_c&fi=29928230&pid=218-83-1-137&up=1.')
-				#print(1)
+				downloadFile('libs\mirai-console-pure-1.0-M4-dev-3.jar',
+				             'https://raw.githubusercontent.com/project-mirai/mirai-repo/master/shadow/mirai-console-pure/mirai-console-pure-1.0-M4-dev-3.jar')
+				# print(1)
 				pass
 			if s == False and adm == 2:
-				downloadFile('libs\mirai-console-1.0-M4-dev-3.jar','https://dev53.baidupan.com/092015bb/2020/09/20/8affec71dec63444a7f82038d7976566.jar?st=zX4Smq2OAda_q_GXbS5i6Q&e=1600589067&b=BjkMZQR2WDxRbQJ5AjEDaAY9WnUMYgVvUmQKKl1rBCgIagglAE1UZQB4UWNVYANxAHgOYVR7BGgHNVgn&fi=29928245&pid=218-83-1-137&up=1.')
-				#print(1)
+				downloadFile('libs\mirai-console-1.0-M4-dev-3.jar',
+				             'https://raw.githubusercontent.com/project-mirai/mirai-repo/master/shadow/mirai-console/mirai-console-1.0-M4-dev-3.jar')
+				# print(1)
 				pass
 			adm += 1
-
 
 		pass
 
